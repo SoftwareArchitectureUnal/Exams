e089d5f0b017f17dbe9cc54a0ce2a68340014dc8
Yeison Garcia <yedgarciago@gmail.com>
2016-02-21 15:35:31 -0500

    add code of Andres for login and signUp

---
+++ src/main/java/com/unal/exams/DataAccess/DAO/ExamsDAO.java
@@ -0,0 +1,16 @@
+package com.unal.exams.DataAccess.DAO;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author AndresGutierrez
+ */
+public class ExamsDAO {
+    
+}
---
+++ src/main/java/com/unal/exams/DataAccess/DAO/UsersDAO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unal.exams.DataAccess.DAO;
+
+import com.unal.exams.DataAccess.Entity.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author AndresGutierrez
+ */
+public class UsersDAO {
+     public EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersistenceUnitExams");
+   public Users persist(Users user){
+       EntityManager em = emf.createEntityManager();
+       em.getTransaction().begin();
+       try{
+           em.persist(user);
+           em.getTransaction().commit();
+       }catch(Exception e){
+           em.getTransaction().rollback();
+           return null;
+       }finally{
+           return user;
+       }
+   }
+   
+   public Users searchUserByUsername(String username){
+       EntityManager em = emf.createEntityManager();
+       Users users = null;
+       try{
+           
+           users = em.find(Users.class, username);
+       }catch(Exception e){}
+       finally{
+           return users;
+       }
+   }
+    
+}
